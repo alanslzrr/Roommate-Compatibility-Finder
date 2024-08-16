f2d72923a5a3ec1f3a513a13a14e013a7d9784c9
Alan Salazar <123749507+alanslzrr@users.noreply.github.com>
2024-08-16 22:17:43 +0200

    ayudantes.py

--- ayudantes.py
+++ ayudantes.py
@@ -3,28 +3,28 @@ import seaborn as sns
 import plotly.graph_objs as go
 import streamlit as st
 
-# FUNCIÓN PARA GENERAR EL GRÁFICO DE COMPATIBILIDAD
-def generar_grafico_compatibilidad(compatibilidad):
-    compatibilidad = compatibilidad / 100  # Asegúrate de que esté en escala de 0 a 1 para porcentajes
+# FUNCTION TO GENERATE THE COMPATIBILITY GRAPH
+def generate_compatibility_graph(compatibility):
+    compatibility = compatibility / 100  # Ensure it's on a scale of 0 to 1 for percentages
     
-    # Configuramos el gráfico de Seaborn
-    fig, ax = plt.subplots(figsize=(5, 4))  # Ajusta el tamaño del gráfico según tus necesidades
+    # Configure the Seaborn graph
+    fig, ax = plt.subplots(figsize=(5, 4))  # Adjust the graph size as needed
     
-    # Crea el gráfico de barras con los valores convertidos a porcentajes
-    sns.barplot(x=compatibilidad.index, y=compatibilidad.values, ax=ax, color='lightblue', edgecolor=None)
+    # Create the bar graph with values converted to percentages
+    sns.barplot(x=compatibility.index, y=compatibility.values, ax=ax, color='lightblue', edgecolor=None)
     
-    # Quitar bordes
+    # Remove borders
     sns.despine(top=True, right=True, left=True, bottom=False)
     
-    # Configurar las etiquetas de los ejes y rotar las etiquetas del eje x
-    ax.set_xlabel('Identificador de Inquilino', fontsize=10)
-    ax.set_ylabel('Similitud (%)', fontsize=10)
+    # Configure axis labels and rotate x-axis labels
+    ax.set_xlabel('Tenant Identifier', fontsize=10)
+    ax.set_ylabel('Similarity (%)', fontsize=10)
     ax.set_xticklabels(ax.get_xticklabels(), rotation=45)
     
-    # Ajustar las etiquetas del eje y para mostrar porcentajes correctamente
+    # Adjust y-axis labels to display percentages correctly
     ax.set_yticklabels(['{:.1f}%'.format(y * 100) for y in ax.get_yticks()], fontsize=8)
-
-    # Añadir etiquetas de porcentaje sobre cada barra
+    
+    # Add percentage labels above each bar
     for p in ax.patches:
         height = p.get_height()
         ax.annotate('{:.1f}%'.format(height * 100),
@@ -32,48 +32,42 @@ def generar_grafico_compatibilidad(compatibilidad):
                     ha='center', va='center',
                     xytext=(0, 5),
                     textcoords='offset points', fontsize=8)
+    return fig
 
-    return(fig)
-
-
-# FUNCIÓN PARA GENERAR LA TABLA DE COMPAÑEROS
-def generar_tabla_compatibilidad(resultado):
-    # Cambiar el nombre de la columna 'index' y ajustar el ancho de las columnas
-    resultado_0_with_index = resultado[0].reset_index()
-    resultado_0_with_index.rename(columns={'index': 'ATRIBUTO'}, inplace=True)
+# FUNCTION TO GENERATE THE ROOMMATES TABLE
+def generate_compatibility_table(result):
+    # Change the name of the 'index' column and adjust column widths
+    result_0_with_index = result[0].reset_index()
+    result_0_with_index.rename(columns={'index': 'ATTRIBUTE'}, inplace=True)
     
-    # Configurar la tabla de Plotly
+    # Configure the Plotly table
     fig_table = go.Figure(data=[go.Table(
-        columnwidth = [20] + [10] * (len(resultado_0_with_index.columns) - 1),  # Ajustar el primer valor para el ancho de la columna 'ATRIBUTO'
-        header=dict(values=list(resultado_0_with_index.columns),
+        columnwidth = [20] + [10] * (len(result_0_with_index.columns) - 1),  # Adjust the first value for the width of the 'ATTRIBUTE' column
+        header=dict(values=list(result_0_with_index.columns),
                     fill_color='paleturquoise',
                     align='left'),
-        cells=dict(values=[resultado_0_with_index[col] for col in resultado_0_with_index.columns],
+        cells=dict(values=[result_0_with_index[col] for col in result_0_with_index.columns],
                    fill_color='lavender',
                    align='left'))
     ])
     
-    # Configurar el layout de la tabla de Plotly
+    # Configure the layout of the Plotly table
     fig_table.update_layout(
-        width=700, height=320,  # Ajustar según tus necesidades
+        width=700, height=320,  # Adjust as needed
         margin=dict(l=0, r=0, t=0, b=0)
     )
+    return fig_table
 
-    return(fig_table)
-
-
-#FUNCIÓN PARA GENERAR LA LISTA DE INQUILINOS SEMILLA
-def obtener_id_inquilinos(inquilino1, inquilino2, inquilino3, topn):
-    # Crea una lista con los identificadores de inquilinos ingresados y los convierte a enteros
-    id_inquilinos = []
-    for inquilino in [inquilino1, inquilino2, inquilino3]:
+# FUNCTION TO GENERATE THE LIST OF SEED TENANTS
+def get_tenant_ids(tenant1, tenant2, tenant3, topn):
+    # Create a list with the entered tenant identifiers and convert them to integers
+    tenant_ids = []
+    for tenant in [tenant1, tenant2, tenant3]:
         try:
-            if inquilino:  # Si hay algún texto en el input
-                id_inquilinos.append(int(inquilino))  # Convierte a entero y agrega a la lista
+            if tenant:  # If there's any text in the input
+                tenant_ids.append(int(tenant))  # Convert to integer and add to the list
         except ValueError:
-            st.error(f"El identificador del inquilino '{inquilino}' no es un número válido.")
-            id_inquilinos = []  # Vaciar la lista si hay un error
-            break  # Salir del bucle
-
-    return(id_inquilinos)
-
+            st.error(f"The tenant identifier '{tenant}' is not a valid number.")
+            tenant_ids = []  # Empty the list if there's an error
+            break  # Exit the loop
+    return tenant_ids
